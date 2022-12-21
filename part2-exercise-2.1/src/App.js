1b66f9b4309976f56752944659b201fce33f4d43
Pragati Poudel <pragati.poudel37@gmail.com>
2022-12-21 09:28:03 -0500

    Add Part2-2-and-2-3

--- part2-exercise-2.1/src/App.js
+++ part2-exercise-2.1/src/App.js
@@ -6,28 +6,36 @@ const Header = (props) => {
 
 const Part = ({part}) => {
   return (
-    <li key={part.id}>{part.name} {part.exercises}</li>
+    <p>{part.name} {part.exercises}</p>
   )
 }
 
 
 const Content = ({coursePart}) => {
   return (
-    <ul>
+    <>
       {coursePart.map(part => (
-        <Part part={part} />
+        <Part key={part.id} part={part} />
       ))}
-    </ul>
+    </>
   )
 }
 
 const Course = (props) => (
   <div>
-  <Header courseName={props.course.name} />
-  <Content coursePart={props.course.parts} />
+    <Header courseName={props.course.name} />
+    <Content coursePart={props.course.parts} />
+    <Total coursePart={props.course.parts} />
   </div>
 )
 
+const Total = ({coursePart}) => {
+    const exercise = coursePart.map((part) => (part.exercises))
+    const sum = exercise.reduce((partialSum, a) => partialSum + a, 0)
+    return (
+      <p><strong>total of {sum} exercises</strong></p>
+    )
+}
 
 const App = () => {
   const course = {
