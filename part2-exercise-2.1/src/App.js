0c943a4cebc1934acac12760ab6b4213914cb4e2
Pragati Poudel <pragati.poudel37@gmail.com>
2022-12-21 09:59:09 -0500

    Add Part2-4

--- part2-exercise-2.1/src/App.js
+++ part2-exercise-2.1/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-      <h1>{props.courseName}</h1>
+      <h2>{props.courseName}</h2>
   )
 }
 
@@ -38,32 +38,59 @@ const Total = ({coursePart}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+  const courses = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10,
-      id:1,
-    },
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
     {
-      name: 'Using props to pass data',
-      exercises: 7,
+      name: 'Node.js',
       id: 2,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-      id: 3,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
-  ] }
+  ]
 
 
   return (
 
     <div>
-      <Course course={course} />
+      <h1>Half Stack application development</h1>
+      {courses.map((course)=>(
+        <Course key={course.id} course={course} />
+      ))}
     </div>
 )}
 
