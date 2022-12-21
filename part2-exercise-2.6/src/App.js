a3ce643a35232c4361ab7f245b999e66d0afd916
Pragati Poudel <pragati.poudel37@gmail.com>
2022-12-21 13:31:49 -0500

    Add Part2-6

---
+++ part2-exercise-2.6/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{name: 'Arto Hellas',
+                                            }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const ShowName = ({name}) => {
+    return (<p>{name}</p>)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+    
+      {persons.map(name =>
+        <ShowName key={name.name} name={name.name} />
+        )}
+    
+    </div>
+  )
+}
+export default App;
