352620b3fd9cfa1f5997dcabce55f4f8e00fdde8
Pragati Poudel <pragati.poudel37@gmail.com>
2022-12-28 16:51:00 -0500

    Add Part 2-14

--- part2-exercise-2.12/src/components/details.js
+++ part2-exercise-2.12/src/components/details.js
@@ -1,4 +1,18 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
 const Details = ({country}) => {
+    const [weather, setWeather] = useState();
+    
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`)
+            .then(response => {
+                setWeather(response.data)
+
+            })
+    }, [country])
     return(
         <div>
             <h1>{country.name.common}</h1>
@@ -11,6 +25,15 @@ const Details = ({country}) => {
                 ))}
             </ul>
             <img src={country.flags.png} alt={country.flag} />
+            {weather && (
+                <div>
+                    <h2>Weather in {country.capital[0]}</h2>
+                    <p>temperature {weather.main.temp} celsius</p>
+                    <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+                    <p>wind {weather.wind.speed} m/s</p>
+                </div>
+            )}
+
         </div>
 )}
 
