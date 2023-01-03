eb398f9652ba6325152aa4239a04d4986e55bd72
Pragati Poudel <pragati.poudel37@gmail.com>
2023-01-03 15:58:34 -0500

    Correct Part2-17

--- part2-exercise-2.6/src/Components/display.js
+++ part2-exercise-2.6/src/Components/display.js
@@ -1,8 +1,9 @@
 
 const ShowOutput = ({id, name, number, handleDelete}) => {
   const handleDeleteClicker = () => {
+    if (window.confirm('Do you really want to delete this entry?')) {
     handleDelete(id)
-  }
+  }}
     return (
     <div>
       <p>
@@ -17,7 +18,7 @@ const ShowOutput = ({id, name, number, handleDelete}) => {
 const Persons = ({persons, newSearch, handleDelete}) => {
     const personsToShow = (newSearch === '') ? persons : persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
     return(personsToShow.map(name =>
-      <ShowOutput key={name.id} id={name.id} name={name.name} number={name.number} handleDelete={handleDelete} />
+      <ShowOutput key={name.name} id={name.id} name={name.name} number={name.number} handleDelete={handleDelete} />
       ))
 }
 
